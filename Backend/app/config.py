0eaae418569329ecb9cfe9b3c605fc3cde9d6ed2
itsMeRanshul <ranshulkumarjayant@gmail.com>
2025-01-29 19:37:01 +0530

    second last commit

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 # Load environment variables from .env
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY")
-RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
-RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET")
+
+DATABASE_URL = os.getenv("DATABASE_URL", "").strip('"')
+SECRET_KEY = os.getenv("SECRET_KEY","").strip('"')
+RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID","").strip('"')
+RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET","").strip('"')
