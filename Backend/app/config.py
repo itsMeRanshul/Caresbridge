6737c991f10064dab0ddf9701fa991b54641271f
itsMeRanshul <ranshulkumarjayant@gmail.com>
2025-01-29 19:44:37 +0530

    commit

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip('"')
+print(DATABASE_URL)
 SECRET_KEY = os.getenv("SECRET_KEY","").strip('"')
 RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID","").strip('"')
 RAZORPAY_SECRET = os.getenv("RAZORPAY_SECRET","").strip('"')
