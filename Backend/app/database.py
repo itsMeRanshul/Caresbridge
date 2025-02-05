23f2ff0e31a2984575fa05f9e294bd59da1e9f11
itsMeRanshul <ranshulkumarjayant@gmail.com>
2025-02-05 15:25:21 +0530

    db commit

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -3,7 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.config import DATABASE_URL
 
-Engine=create_engine(DATABASE_URL)
+Engine=create_engine(DATABASE_URL,
+    pool_pre_ping=True, 
+    pool_recycle=3600, 
+    pool_size=10,        
+    max_overflow=20,  )
 SessionLocal=sessionmaker(autoflush=False,bind=Engine)
 Base=declarative_base()
 
